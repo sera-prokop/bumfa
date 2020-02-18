857ce60ca905e6d0a9968345676abcd9ca60f91e
sera.prokop <sera.prokop@gmail.com>
2020-02-18 12:24:31 +0300

    test

--- markup/components/main-slider/main-slider.js
+++ markup/components/main-slider/main-slider.js
@@ -1,17 +1,12 @@
 import ready from '../../static/js/documentReady.js';
-
 ready(function() {
-
-    new Swiper('.main-slider__container', {
-        loop: true,
-
-        slidesPerView: '1',
-        navigation: {
-            nextEl: '.main-slider__next',
-            prevEl: '.main-slider__prev',
-        },
-
-    });
-
+new Swiper('.main-slider__container', {
+loop: true,
+slidesPerView: '1',
+navigation: {
+nextEl: '.main-slider__next',
+prevEl: '.main-slider__prev',
+},
+});
 });
 
