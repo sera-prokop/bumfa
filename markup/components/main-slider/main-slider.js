d2cec56b13ce88327f17647bb44739cdf5ea8082
sera.prokop <sera.prokop@gmail.com>
2020-02-18 13:34:43 +0300

    add slider block + favicon

--- markup/components/main-slider/main-slider.js
+++ markup/components/main-slider/main-slider.js
@@ -1,12 +1,16 @@
 import ready from '../../static/js/documentReady.js';
 
 ready(function() {
-    new Swiper('.main-slider__container', {
+    new Swiper('.main-slider__slider', {
         loop: true,
+        keyboard: {
+            enabled: true,
+            // onlyInViewport: false,
+        },
+        grabCursor: true,
         slidesPerView: '1',
         navigation: {
             nextEl: '.main-slider__next',
-
             prevEl: '.main-slider__prev',
         },
     });
