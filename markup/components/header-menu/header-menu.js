726daa9dc1a2d479f7126b30346e5b1b8715f12c
sera.prokop <sera.prokop@gmail.com>
2020-02-19 11:31:06 +0300

    fix for main page

--- markup/components/header-menu/header-menu.js
+++ markup/components/header-menu/header-menu.js
@@ -1,33 +1,32 @@
 import ready from '../../static/js/documentReady.js';
 
 ready(function() {
-    $(document).on('click', function(e) {
-        var div = $('.header-menu__dropdown');
-
-        if (
-            !div.is(e.target) && // если клик был не по нашему блоку
-            div.has(e.target).length === 0
-        ) {
-            // и не по его дочерним элементам
-            // $(this).closest('.header-menu__item--drop').removeClass('show')
-            // div.closest('.header-menu__item--drop').removeClass('show');
-        }
-    });
-
-    $('.header-menu__link').on('click', function(e) {
-        const content = $(this).siblings('.header-menu__dropdown');
-
-        const div = $(e.target.closest('.header-menu__item--drop'));
-
-        // console.log(e.target.closest('.header-menu__item--drop'));
-        if (content.length) {
-            e.preventDefault();
-
-            $(this)
-                .closest('.header-menu__item--drop')
-                .toggleClass('show')
-                .siblings()
-                .removeClass('show');
-        }
-    });
+    // $('.header-menu__link').on('click', function(e) {
+    //     const content = $(this).siblings('.header-menu__dropdown');
+    //
+    //     const div = $(e.target.closest('.header-menu__item--drop'));
+    //
+    //     if (content.length) {
+    //         e.preventDefault();
+    //
+    //         $(this)
+    //             .closest('.header-menu__item--drop')
+    //             .toggleClass('show')
+    //             .siblings()
+    //             .removeClass('show');
+    //     }
+    // });
+    //
+    // window.onclick = function(event) {
+    //     if (!event.target.closest('.header-menu__link')) {
+    //         var dropdowns = document.getElementsByClassName("header-menu__item--drop");
+    //         var i;
+    //         for (i = 0; i < dropdowns.length; i++) {
+    //             var openDropdown = dropdowns[i];
+    //             if (openDropdown.classList.contains('show')) {
+    //                 openDropdown.classList.remove('show');
+    //             }
+    //         }
+    //     }
+    // }
 });
--- markup/components/main-slider/main-slider.js
+++ markup/components/main-slider/main-slider.js
@@ -3,11 +3,14 @@ import ready from '../../static/js/documentReady.js';
 ready(function() {
     new Swiper('.main-slider__slider', {
         loop: true,
+        autoplay: {
+            delay: 3000,
+        },
         keyboard: {
             enabled: true,
             // onlyInViewport: false,
         },
-        grabCursor: true,
+        // grabCursor: true,
         slidesPerView: '1',
         navigation: {
             nextEl: '.main-slider__next',
