0aaa87fee73c9b6ab08a32ca7d4a7d11391da10c
sera.prokop <sera.prokop@gmail.com>
2020-02-26 21:35:52 +0300

    modal

--- markup/components/about-video/about-video.js
+++ markup/components/about-video/about-video.js
@@ -7,7 +7,29 @@ ready(function() {
         $(this)
             .find('video')[0]
             .play();
+    });
 
-        console.log($(this).find('video')[0]);
+    $('.about-video__img').on('click', function(e) {
+        e.preventDefault();
+
+        const dataTime = $(this).data('time');
+        const videoWrapper = $(this)
+            .closest('.about-video')
+            .find('.about-video__big-link');
+        const videoElement = videoWrapper.find('video')[0];
+
+        videoWrapper.addClass('active');
+        if (dataTime) {
+            videoElement.addEventListener(
+                'seeked',
+                function() {
+                    videoElement.play();
+                },
+                true
+            );
+            videoElement.currentTime = dataTime;
+        } else {
+            videoElement.play();
+        }
     });
 });
