2f016dacd238a7256972dcb405a92d39c132ac83
sera.prokop <sera.prokop@gmail.com>
2020-02-26 21:02:46 +0300

    add modal

--- markup/components/about-video/about-video.js
+++ markup/components/about-video/about-video.js
@@ -1,7 +1,8 @@
 import ready from '../../static/js/documentReady.js';
 
 ready(function() {
-    $('.about-video__big-link').on('click', function() {
+    $('.about-video__big-link').on('click', function(e) {
+        e.preventDefault();
         $(this).addClass('active');
         $(this)
             .find('video')[0]
@@ -9,6 +10,4 @@ ready(function() {
 
         console.log($(this).find('video')[0]);
     });
-
-    // $('#videoId').get(0).play()
 });
